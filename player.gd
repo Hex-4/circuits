2e63ad3389f647b1bc5b60eeb38964ed6a7f0610
hex4 <121138331+Hex-4@users.noreply.github.com>
2025-07-31 14:10:01 -0600

    🧃 smooth turning

--- player.gd
+++ player.gd
@@ -25,15 +25,22 @@ extends CharacterBody2D
 @export var gravity = 4000
 @export_range(0.0, 1.0) var friction = 0.06
 @export_range(0.0 , 1.0) var acceleration = 0.08
-
+var dir: Vector2
 
 func _physics_process(delta):
-	var dir = Input.get_vector("left", "right", "up", "down")
+
+	dir = Input.get_vector("left", "right", "up", "down")
 	if dir != Vector2.ZERO:
 		velocity.x = lerp(velocity.x, dir.x * speed, acceleration)
 		velocity.y = lerp(velocity.y, dir.y * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, friction)
 		velocity.y = lerp(velocity.y, 0.0, friction)
+		
+	if dir != Vector2.ZERO:
+		var tween = get_tree().create_tween()
+		var weird_dir = Vector2(dir.x, -dir.y)
+		if rotation != weird_dir.angle_to(Vector2.DOWN):
+			tween.tween_property(self, "rotation", weird_dir.angle_to(Vector2.DOWN), 0.4)
 	
 	move_and_slide()
